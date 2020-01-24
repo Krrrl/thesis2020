179e9a6e6b2bca8c2001eaaf68cbfa5377f5d996
Krrrl <Km.ylvisaker@gmail.com>
2020-01-24 11:02:58 +0100

    Updated __init__.py

--- manipulator_training/gym_environments/envs/__init__.py
+++ manipulator_training/gym_environments/envs/__init__.py
@@ -3,3 +3,7 @@ from gym.envs.registration import register
 register(id='ManipulatorLeverEnv-v0',
 		entry_point='gym_environments.envs.lever_env:ManipulatorLeverEnv'
 )
+
+register(id='minimal_GYM-v0',
+		entry_point='gym_environments.envs.minimal_GYM_env:minimal_GYM'
+)
