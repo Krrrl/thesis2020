0af0c3286a131359653396828f52b5f9e2fadb06
Krrrl <Km.ylvisaker@gmail.com>
2020-01-24 11:03:43 +0100

    Added a minimal_GYM example ENV
    For reference

---
+++ manipulator_training/gym_environments/envs/minimal_GYM_env/README.txt
@@ -0,0 +1,4 @@
+The absolute minimum working GYM environment is shown in the minimal_GYM.py file.
+Copy and rename it to a different directory, and fill in the blanks(usually alot!)
+
+NB! Make sure that you register your new environments in the envs/__init__.py file!
---
+++ manipulator_training/gym_environments/envs/minimal_GYM_env/minimal_GYM.py
@@ -0,0 +1,40 @@
+import gym
+import gym_environments.envs
+
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+#UNCOMMENT if you intend to use the manipulator or GYM-ROS interface.
+#import ros_message_listener.eavesdrop as eavesdrop
+#from ManipulatorAction import ManipulatorAction
+
+class MinimalGYM(gym.Env):
+
+	def __init__(self):
+		#self.action_space = 
+		pass
+
+	def step(self, action):
+
+		return observation, reward, done, info
+
+	def reset(self):
+
+		return observation
+
+	def render(self):
+		pass
+
+	def _get_obs(self):
+
+		return observation
+
+if __name__ == '__main__':
+	
+	#Environment unit test!
+	#Cant be executed before environment is registered with gym, in envs/__init__.py
+	# env = gym.make('MinimalGYM-v0'..)
+
+	# env.step(action_space.get_random_action())
+
+	# env.reset()
